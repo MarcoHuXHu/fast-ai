4b7a7769a2a583b28c20173353c29f5390e7d8a0
MarcoHuXHu <huxhu2333@gmail.com>
2017-11-19 22:38:06 +0800

    update alias.sh

--- setup/aws-alias.sh
+++ setup/aws-alias.sh
@@ -1,14 +1,20 @@
-alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-alias aws-start='aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
-alias aws-ip='export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
-alias aws-stop='aws ec2 stop-instances --instance-ids $instanceId'
-alias aws-state='aws ec2 describe-instances --instance-ids $instanceId --query "Reservations[0].Instances[0].State.Name"'
+echo alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+echo alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+
+aws-get='aws ec2 describe-instances'
+
+export fastId='i-0962b3128935da2ab'
+alias fast-ssh='ssh -i "~/.ssh/aws-key-fast-ai.pem" ubuntu@ec2-34-214-81-120.us-west-2.compute.amazonaws.com'
+alias fast-get='echo $fastId'
+alias fast-ip='export fastIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$fastId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $fastIp'
+alias fast-start='aws ec2 start-instances --instance-ids $fastId && aws ec2 wait instance-running --instance-ids $fastId && export fastIP=`aws ec2 describe-instances --filters "Name=instance-id,Values=$fastId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $fastIp'
+alias fast-stop='aws ec2 stop-instances --instance-ids $fastId'
+alias fast-state='aws ec2 describe-instances --instance-ids $fastId --query "Reservations[0].Instances[0].State.Name"'
 
 export huxhuId='i-0da06dc6ad1cfa17e'
 alias huxhu-ssh='ssh -i ~/.ssh/aws-key-huxhu-tech.pem ec2-user@ec2-34-216-5-46.us-west-2.compute.amazonaws.com'
 alias huxhu-get='echo $huxhuId'
-alias huxhu-start='aws ec2 start-instances --instance-ids $huxhuId && aws ec2 wait instance-running --instance-ids $huxhuId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$huxhuId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp'
+alias huxhu-start='aws ec2 start-instances --instance-ids $huxhuId && aws ec2 wait instance-running --instance-ids $huxhuId && export huxhuIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$huxhuId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $huxhuIp'
 alias huxhu-stop='aws ec2 stop-instances --instance-ids $huxhuId'
 alias huxhu-state='aws ec2 describe-instances --instance-ids $huxhuId --query "Reservations[0].Instances[0].State.Name"'
 
@@ -17,17 +23,17 @@ alias huxhu-state='aws ec2 describe-instances --instance-ids $huxhuId --query "R
 if [[ `uname` == *"CYGWIN"* ]]
 then
     # This is cygwin.  Use cygstart to open the notebook
-    alias aws-nb='cygstart http://$instanceIp:8888'
+    alias aws-nb='cygstart http://$fastIp:8888'
 fi
 
 if [[ `uname` == *"Linux"* ]]
 then
     # This is linux.  Use xdg-open to open the notebook
-    alias aws-nb='xdg-open http://$instanceIp:8888'
+    alias aws-nb='xdg-open http://$fastIp:8888'
 fi
 
 if [[ `uname` == *"Darwin"* ]]
 then
     # This is Mac.  Use open to open the notebook
-    alias aws-nb='open http://$instanceIp:8888'
+    alias aws-nb='open http://$fastIp:8888'
 fi
