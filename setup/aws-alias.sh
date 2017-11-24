1164389a48c71a5653656899d6cb13cb23247766
MarcoHuXHu <huxhu2333@gmail.com>
2017-11-24 10:14:20 +0800

    update for upload

--- setup/aws-alias.sh
+++ setup/aws-alias.sh
@@ -1,7 +1,7 @@
-echo alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
-echo alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+echo aws-alias loaded
 
-aws-get='aws ec2 describe-instances'
+alias aws-get-p2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
+alias aws-get-t2='export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.micro" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId'
 
 export fastId='i-0962b3128935da2ab'
 alias fast-ssh='ssh -i "~/.ssh/aws-key-fast-ai.pem" ubuntu@ec2-34-214-81-120.us-west-2.compute.amazonaws.com'
@@ -11,6 +11,10 @@ alias fast-start='aws ec2 start-instances --instance-ids $fastId && aws ec2 wait
 alias fast-stop='aws ec2 stop-instances --instance-ids $fastId'
 alias fast-state='aws ec2 describe-instances --instance-ids $fastId --query "Reservations[0].Instances[0].State.Name"'
 
+fast-upload() {
+	scp -i ~/.ssh/aws-key-fast-ai.pem "$1" ubuntu@ec2-34-214-81-120.us-west-2.compute.amazonaws.com:~/upload/"$2"
+}
+
 export huxhuId='i-0da06dc6ad1cfa17e'
 alias huxhu-ssh='ssh -i ~/.ssh/aws-key-huxhu-tech.pem ec2-user@ec2-34-216-5-46.us-west-2.compute.amazonaws.com'
 alias huxhu-get='echo $huxhuId'
@@ -19,7 +23,9 @@ alias huxhu-start='aws ec2 start-instances --instance-ids $huxhuId && aws ec2 wa
 alias huxhu-stop='aws ec2 stop-instances --instance-ids $huxhuId'
 alias huxhu-state='aws ec2 describe-instances --instance-ids $huxhuId --query "Reservations[0].Instances[0].State.Name"'
 
-
+huxhu-upload() {
+	scp -i ~/.ssh/aws-key-huxhu-tech.pem "$1" ec2-user@ec2-34-216-5-46.us-west-2.compute.amazonaws.com:~/upload/"$2"
+}
 
 if [[ `uname` == *"CYGWIN"* ]]
 then
